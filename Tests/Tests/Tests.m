293b9d585b0361715103a983619d226d5cfc5fc6
Travis Jeffery <tj@travisjeffery.com>
2014-06-15 23:17:11 -0400

    Add tests

--- Tests/Tests/Tests.m
+++ Tests/Tests/Tests.m
@@ -7,28 +7,41 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "TRVSURLSessionOperation.h"
 
 @interface Tests : XCTestCase
+
+@property (nonatomic, strong) NSOperationQueue *queue;
+@property (nonatomic, strong) NSURLSession *session;
 
 @end
 
 @implementation Tests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    _queue = [[NSOperationQueue alloc] init];
+    _queue.maxConcurrentOperationCount = 1;
+    _session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration]];
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+- (void)test {
+    NSArray *urls = @[ [NSURL URLWithString:@"https://twitter.com/travisjeffery"], [NSURL URLWithString:@"https://github.com/travisjeffery"] ];
+    NSMutableArray *result = [[NSMutableArray alloc] init];
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [_queue addOperation:[[TRVSURLSessionOperation alloc] initWithSession:_session request:[NSURLRequest requestWithURL:urls[0]] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        XCTAssert(result.count == 0);
+        [result addObject:response.URL];
+    }]];
+
+    [_queue addOperation:[[TRVSURLSessionOperation alloc] initWithSession:_session URL:urls[1] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        [result addObject:response.URL];
+    }]];
+
+    [_queue waitUntilAllOperationsAreFinished];
+
+    XCTAssertEqualObjects(urls, result);
 }
 
 @end
